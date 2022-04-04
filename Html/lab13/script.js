718f85579ac91e686deea3a45d411f7f0fb7b828
zav0dila <andriysmyk2003@gmail.com>
2022-04-04 11:58:25 +0300

    lab13 done

---
+++ Html/lab13/script.js
@@ -0,0 +1,124 @@
+//Завдання 1
+let arr = [];
+
+arr[0] = 7;
+arr[1]= "Hello";
+arr[2] = true;
+arr[3] = null;
+
+console.log(arr.length);
+arr[4] = prompt("Введіть будь-яке значення");
+console.log(arr[4]);
+arr.shift();
+console.log(arr);
+
+//Завдання 2
+let cities=["Ternopil", "Lviv", "Warsaw"];
+console.log(cities.join("*"));
+
+//Завдання 3
+let numbers=[2,3,4,5];
+let multiply=1;
+for (let i=0; i<numbers.length; i++){
+    multiply*=numbers[i];
+}
+console.log(multiply);
+multiply=1;
+let i=0;
+while (numbers[i]!==undefined){
+    multiply*=numbers[i];
+    i++;
+}
+console.log(multiply);
+
+//Завдання 4
+i=0;
+for(let i=0; i<=15; i++){
+    if (i%2===0){
+        console.log(i+" is even");
+    }
+    else {
+        console.log(i+" is odd");
+    }
+}
+
+//Завдання 5
+function randArray(k){
+    let arr=[];
+    for (let i=0; i<k; i++){
+        arr[i]=Math.floor(Math.random()*100);
+    }
+    return arr;
+}
+
+let arr2=randArray(10);
+console.log(arr2);
+
+//Завдання 6
+let number=prompt("Введіть число");
+let degree=prompt("Введіть степінь");
+function raiseToDegree(number, degree){
+    let result=1;
+    for (let i=0; i<degree; i++){
+        result*=number;
+    }
+    return result;
+}
+console.log(raiseToDegree(number, degree));
+
+//Завдання 7
+function findMin(){
+    let min=arguments[0];
+    for (let i=0; i<arguments.length; i++){
+        if (arguments[i]<min){
+            min=arguments[i];
+        }
+    }
+    return min;
+}
+
+console.log(findMin(7.6,-5.7,0.4,11.1,0.1));
+
+//Завдання 8
+function findUnique(){
+    for (let i=0; i<arguments.length; i++){
+        for (let j=i+1; j<arguments.length; j++){
+            if (arguments[i]===arguments[j]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+console.log(findUnique(3,8,13,18,23));
+console.log(findUnique(3,8,13,13,23));
+
+//Завдання 9
+function lastElement(arr, n){
+    let returnArr=[];
+    if(n===undefined){
+        return arr[arr.length-1];
+    }
+    else{
+        for (let i=arr.length-n; i<arr.length; i++){
+            returnArr.push(arr[i]);
+        }
+        return returnArr;
+    }
+}
+
+let example=[1,2,3,4,5,6,7,8,9,10];
+console.log(lastElement(example));
+console.log(lastElement(example,7));
+
+//Завдання 10
+function firstLettersToUpperCase(str){
+    let arr=str.split(" ");
+    for (let i=0; i<arr.length; i++){
+        arr[i]=arr[i].charAt(0).toUpperCase()+arr[i].slice(1);
+    }
+    return arr.join(" ");
+}
+
+console.log(firstLettersToUpperCase("hello world"));
